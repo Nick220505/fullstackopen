c67429a9af2f880e1cdfc7a7a58c4b8874cc27fd
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-05-30 14:40:50 -0500

    Error handling added

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -7,15 +7,13 @@ const cors = require('cors')
 
 morgan.token('data', req => (req.method === 'POST') ? JSON.stringify(req.body) : ' ')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+    Person.find({}).then(people => response.json(people))
 })
 
 app.get('/info', (request, response) => {
@@ -27,20 +25,16 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id).then(deletedPerson => {
-        response.status(204).end()
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -52,35 +46,39 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    Person.findOne({ name: body.name }).then(foundPerson => {
-        if (!foundPerson) {
-            const person = new Person({
-                name: body.name,
-                number: body.number,
-            })
-            person.save().then(savedPerson => {
-                response.json(person)
-            })
-        } else {
-            response.status(400).json({
-                'error': 'name must be unique'
-            })
-        }
-    })
+    Person.findOne({ name: body.name })
+        .then(foundPerson => {
+            if (!foundPerson) {
+                const person = new Person({
+                    name: body.name,
+                    number: body.number,
+                })
+                person.save().then(savedPerson => {
+                    response.json(person)
+                })
+            } else {
+                response.status(400).json({
+                    'error': 'name must be unique'
+                })
+            }
+        })
 })
 
-app.put('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-
-    if (!id) {
-        return response.status(400).json({
-            'error': 'id missing'
+app.put('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .then(updatedPerson => {
+            if (!updatedPerson) {
+                response.status(404).end()
+            }
+            response.json(updatedPerson)
         })
-    }
+        .catch(error => next(error))
+})
 
-    Person.findByIdAndUpdate(id, request.body).then(updatedPerson => {
-        response.json(updatedPerson)
-    })
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(() => response.status(204).end())
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -89,7 +87,17 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
