feb26ef3e58b85bbbd5b3a84c09e122fa21e925d
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-05-30 18:54:28 -0500

    Validators added for both name and number

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -37,41 +37,20 @@ app.get('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            'error': 'name or number is missing'
-        })
-    }
-
-    Person.findOne({ name: body.name })
-        .then(foundPerson => {
-            if (!foundPerson) {
-                const person = new Person({
-                    name: body.name,
-                    number: body.number,
-                })
-                person.save().then(savedPerson => {
-                    response.json(person)
-                })
-            } else {
-                response.status(400).json({
-                    'error': 'name must be unique'
-                })
-            }
-        })
+app.post('/api/persons', (request, response, next) => {
+    const person = new Person(request.body)
+    person.save()
+        .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
-        .then(updatedPerson => {
-            if (!updatedPerson) {
-                response.status(404).end()
-            }
-            response.json(updatedPerson)
-        })
+    Person.findByIdAndUpdate(
+        request.params.id,
+        request.body,
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => response.json(updatedPerson))
         .catch(error => next(error))
 })
 
@@ -92,6 +71,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError' || error.name === 'MongoServerError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
