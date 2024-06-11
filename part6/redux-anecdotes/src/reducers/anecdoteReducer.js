49fba4d819f5461579848148ee7be4bd6eb1fd34
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-06-11 01:14:49 -0500

    json-server added and services implemented

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,16 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
 const orderByNumberOfVotes = anecdotes => {
   return anecdotes.sort((anecdoteA, anecdoteB) => (
     anecdoteB.votes - anecdoteA.votes
@@ -19,16 +8,10 @@ const orderByNumberOfVotes = anecdotes => {
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState: anecdotesAtStart.map(content => (
-    { id: getId(), content, votes: 0 }
-  )),
+  initialState: [],
   reducers: {
     createAnecdote: (state, action) => {
-      state.push({
-        id: getId(),
-        content: action.payload,
-        votes: 0
-      })
+      state.push(action.payload)
     },
     increaseVotesOf: (state, action) => {
       return orderByNumberOfVotes(state.map(anecdote => {
@@ -40,9 +23,10 @@ const anecdoteSlice = createSlice({
         }
         return anecdote
       }))
-    }
+    },
+    setAnecdotes: (state, action) => action.payload
   }
 })
 
-export const { createAnecdote, increaseVotesOf } = anecdoteSlice.actions
+export const { createAnecdote, increaseVotesOf, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
