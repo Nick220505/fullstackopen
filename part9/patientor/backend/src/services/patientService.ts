994ac82f430b34942072e24bcaea9c37a9458ea5
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-06-22 18:50:05 -0500

    POST endpoint for patients added

--- part9/patientor/backend/src/services/patientService.ts
+++ part9/patientor/backend/src/services/patientService.ts
@@ -1,4 +1,9 @@
-import { NonSensitivePatientEntry, PatientEntry } from '../types';
+import { v1 as uuid } from 'uuid';
+import {
+  NonSensitivePatientEntry,
+  PatientEntry,
+  NewPatientEntry,
+} from '../types';
 import patientEntries from '../../data/patients';
 
 const getEntries = (): PatientEntry[] => {
@@ -17,4 +22,13 @@ const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
   );
 };
 
-export default { getEntries, getNonSensitiveEntries };
+const addEntry = (object: NewPatientEntry): PatientEntry => {
+  const newPatient: PatientEntry = {
+    id: uuid(),
+    ...object,
+  };
+  patientEntries.push(newPatient);
+  return newPatient;
+};
+
+export default { getEntries, getNonSensitiveEntries, addEntry };
